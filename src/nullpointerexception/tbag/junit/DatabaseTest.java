0d92730776e2bf3fa8c2f3d35834308e33383fe1
Ian Arnold <iarnold1@ycp.edu>
2021-05-07 22:29:58 -0400

    junit test

---
+++ src/nullpointerexception/tbag/junit/DatabaseTest.java
@@ -0,0 +1,14 @@
+package nullpointerexception.tbag.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DatabaseTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
