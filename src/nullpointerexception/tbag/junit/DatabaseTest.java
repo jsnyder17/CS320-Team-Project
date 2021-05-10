5b5a21778b9883f7b810caa5d3167bfe63db6797
Ian Arnold <iarnold1@ycp.edu>
2021-05-10 08:58:57 -0400

    junit tests

--- src/nullpointerexception/tbag/junit/DatabaseTest.java
+++ src/nullpointerexception/tbag/junit/DatabaseTest.java
@@ -1,14 +1,95 @@
 package nullpointerexception.tbag.junit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class DatabaseTest {
+import nullpointerexception.tbag.actors.Player;
+import nullpointerexception.tbag.persist.DerbyDatabase;
+import nullpointerexception.tbag.rooms.Room;
 
+class DatabaseTest {
+	DerbyDatabase db = new DerbyDatabase();
+	
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testOutPut() {
+		System.out.println("creating tables");
+		db.setUsername("ian");
+		db.dropTables();
+		db.createTables();
+		System.out.println("creating tables");
+		db.loadInitialData();
+		System.out.println("loading data");
+		
+		ArrayList<String> inPut = new ArrayList<String>();
+		inPut.add("move");
+		inPut.add("go");
+		
+		db.addOutput(inPut);
+		ArrayList<String> outPut = new ArrayList<String>();
+		outPut.add("move");
+		outPut.add("go");
+		ArrayList<String> test = db.getOutput();
+		assertEquals(test, outPut);
 	}
+	
+	@Test
+	public void testGetRooms() {
+		System.out.println("creating tables");
+		db.setUsername("ian");
+		db.dropTables();
+		db.createTables();
+		System.out.println("creating tables");
+		db.loadInitialData();
+		System.out.println("loading data");
+		
+		ArrayList<Room> rooms = db.getRooms(); 
+		assertEquals(3, rooms.get(2).getRoomId());
+		assertEquals(false, rooms.get(0).getNorthDoor().getIsUnlocked());
+	}
+	
+	@Test
+	public void testMovePlayer() {
+		System.out.println("creating tables");
+		db.setUsername("ian");
+		db.dropTables();
+		db.createTables();
+		System.out.println("creating tables");
+		db.loadInitialData();
+		System.out.println("loading data");
+		
+		db.movePlayer(3);
+		assertEquals(3, db.getPlayer().getCurrentRoom());
+	}
+	
+	@Test
+	public void testGetIndividualNpc() {
+		
+		System.out.println("creating tables");
+		db.setUsername("ian");
+		db.dropTables();
+		db.createTables();
+		System.out.println("creating tables");
+		db.loadInitialData();
+		System.out.println("loading data");
+		
+		Player player = db.getPlayer();
+		player.setHealth(50);
+		player.setEquippedWeaponIndex(0);
+		
+		db.updatePlayer(player);
+		
+		assertEquals(50, db.getPlayer().getHealth());
+		assertEquals(0, db.getPlayer().getEquippedWeaponIndex());
+	}
+	
+	
 
 }
